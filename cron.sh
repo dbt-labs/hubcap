bb4179d0240ecb59f68bb85683d2095ce8a2cc8e
Drew Banin <drew@fishtownanalytics.com>
2020-06-09 12:31:47 -0400

    set git username in cron script

--- cron.sh
+++ cron.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+git config --global user.email "drew@fishtownanalytics.com"
+git config --global user.name "Hubcap"
+
 curl https://raw.githubusercontent.com/fishtown-analytics/hubcap/master/hub.json > hub.current.json
 echo ${CONFIG} > config.json
 rm -rf git-tmp/ROOT

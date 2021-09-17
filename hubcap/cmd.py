51269e4946127620993b8720515de865b66088e7
Mila Page <versusfacit@users.noreply.github.com>
2021-09-17 03:03:57 -0700

    Decoupling code nearly completed. Added default-branch flexibility.

--- hubcap/cmd.py
+++ hubcap/cmd.py
@@ -1,8 +1,12 @@
 
 import logging
+import os
 import subprocess
 import sys
 
+from git import cmd
+from git import Repo
+from pathlib import Path
 
 def run_cmd(cmd, quiet=False):
     proc = subprocess.run(args=cmd.split(), capture_output=True)
@@ -18,3 +22,25 @@ def run_cmd(cmd, quiet=False):
     proc.check_returncode()
 
     return output
+
+
+def repo_default_branch(repo):
+    prev_dir = os.getcwd()
+    os.chdir(repo.working_dir)
+
+    remote_info = cmd.Git().remote('show', 'origin')
+    default_branch_name = remote_info.split('\n')[3].split()[-1]
+
+    os.chdir(prev_dir)
+    return default_branch_name
+
+
+def clone_repo(remote, path):
+    '''clone down a github repo to a path and a reference to that directory'''
+    logging.info(f'cloning {remote} to {path}')
+    repo = Repo.clone_from(remote, path)
+
+    main_branch = repo_default_branch(repo)
+    repo.git.checkout(main_branch)
+    repo.remotes.origin.pull()
+    return path
--- hubcap/hubcap.py
+++ hubcap/hubcap.py
@@ -5,7 +5,10 @@ import requests
 
 from pathlib import Path
 
-from setup import *
+import setup
+import version
+import package
+
 from cmd import *
 
 # ==
@@ -13,113 +16,37 @@ from cmd import *
 # ==
 
 logging.info('preparing script state')
-config = build_config()
+config = setup.build_config()
 
 ONE_BRANCH_PER_REPO = config['one_branch_per_repo']
 PUSH_BRANCHES = config['push_branches']
-REMOTE = config['remote']
+REMOTE = 'https://github.com/dbt-labs/hub.getdbt.com.git'
 TMP_DIR = os.environ['GIT_TMP']
-TRACKED_REPOS = config['tracked_repos']
+TRACKED_REPOS = setup.load_tracked_repo_records()
 
-# ==
-# == Pull down the newest version of the hub repo
-# ==
+# pull down hub to assess current state and have ready for future commits
+hub_dir_path = clone_repo(REMOTE, TMP_DIR / Path('hub'))
 
-hub_dir = TMP_DIR / Path('hub')
-logging.info(f'cloning hub site repo to {hub_dir}')
-run_cmd(f'git clone --quiet {REMOTE} {hub_dir}')
-os.chdir(hub_dir)
-run_cmd('git checkout master')
-run_cmd('git pull origin master')
+# create a record in memory of what versions are already committed into the hub
+existing_pkg_version_index = setup.build_pkg_version_index(hub_dir_path)
 
-index = build_version_index()
-new_branches = {}
+# =
+# = Determine new package versions
+# =
 
-# ==
-# == Cut a branch for each new version of each package
-# ==
+logging.info('cloning package repos')
+package.clone_package_repos(TRACKED_REPOS, TMP_DIR)
 
-from version import *
-from package import *
+logging.info('collecting the new version tags for packages checked into hub')
+pkgs_with_updates = package.get_pkgs_with_updates(TRACKED_REPOS, existing_pkg_version_index, TMP_DIR)
+
+# =
+# = Create new specs and commit them to separate branches in hub
+# =
 
 logging.info('preparing branches for packages with versions to be added')
-for org_name, repos in TRACKED_REPOS.items():
-    for repo in repos:
-        os.chdir(TMP_DIR)
-        current_repo_path = TMP_DIR / Path(repo)
-
-        logging.info(f'cloning package {repo} maintained by {org_name} to {current_repo_path}')
-        clone_url = f'https://github.com/{org_name}/{repo}.git'
-        run_cmd(f'git clone --quiet {clone_url} {repo}')
-
-        # skip repo if no dbt project yaml or no new tags
-        if not has_dbt_project_yml(current_repo_path):
-            logging.warning(f'{repo} has no dbt_project.yml. Skipping...')
-            continue
-
-        package_name = parse_pkg_name(current_repo_path)
-        packages = parse_pkgs(current_repo_path)
-
-        os.chdir(current_repo_path)
-        logging.info(f'collecting tags for {package_name}')
-        new_tags = get_new_tags(index[org_name][package_name])
-
-        if new_tags:
-            logging.info(f'new tags:    {sorted(new_tags)}')
-        else:
-            logging.warning(f'no new tags for {repo}. Skipping...')
-            continue
-
-        # =
-        # = Create new specs and commit them to separate branches in hub
-        # =
-
-        os.chdir(hub_dir) # will not change until tag loop
-        repo_dir = hub_dir / 'data' / 'packages' / org_name / package_name / 'versions'
-        Path.mkdir(repo_dir, parents=True, exist_ok=True)
-
-        # in hubcap, on a branch for each package, commit the package version specs for any new tags
-        branch_name = cut_version_branch(org_name, repo, ONE_BRANCH_PER_REPO)
-        new_branches[branch_name] = {"org": org_name, "repo": package_name}
-
-        # create an updated version of the repo's index.json
-        index_file_path = hub_dir / 'data' / 'packages' / org_name / package_name / 'index.json'
-
-        new_index_entry = make_index(
-            org_name,
-            repo,
-            package_name,
-            fetch_index_file_contents(index_file_path),
-            new_tags | get_existing_tags(index[org_name][package_name]),
-            current_repo_path
-        )
-
-        with open(index_file_path, 'w') as f:
-            logging.info(f'writing index.json to {index_file_path}')
-            f.write(str(json.dumps(new_index_entry, indent=4)))
-
-        # create a version spec for each tag
-        for tag in new_tags:
-            package_spec = make_spec(org_name, repo, package_name, packages, tag, current_repo_path)
-
-            version_path = repo_dir / Path(f'{tag}.json')
-
-            with open(version_path, 'w') as f:
-                logging.info(f'writing spec to {version_path}')
-                f.write(str(json.dumps(package_spec, indent=4)))
-
-            # checkout tag within current package repo and create JSON spec
-            os.chdir(repo_dir)
-            msg = f'hubcap: Adding tag {tag} for {org_name}/{repo}'
-            logging.info(msg)
-            run_cmd('git add -A')
-            subprocess.run(args=['git', 'commit', '-am', f'{msg}'], capture_output=True)
-            new_branches[branch_name]['new'] = True
-
-        # good house keeping
-        os.chdir(hub_dir)
-        run_cmd('git checkout master')
-
+# this wants to take place inside the git-tmp/hub repo
+new_branches = package.commit_version_updates_to_hub(pkgs_with_updates, hub_dir_path)
 
 # =
 # = push new branches, if there are any

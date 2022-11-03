9d196b9432573ab2df467fda9a54a95651293788
Doug Beatty <doug.beatty@dbtlabs.com>
2022-11-03 14:21:27 -0600

    Reformatting via black

--- hubcap/git_helper.py
+++ hubcap/git_helper.py
@@ -1,4 +1,4 @@
-'''Interface for invoking CLI executables safely from inside this script'''
+"""Interface for invoking CLI executables safely from inside this script"""
 
 import logging
 import os
@@ -12,13 +12,13 @@ from pathlib import Path
 
 
 def run_cmd(cmd, quiet=False):
-    '''Dispatching commands to shell from inside Python needs an exit status wrapper'''
+    """Dispatching commands to shell from inside Python needs an exit status wrapper"""
     proc = subprocess.run(args=cmd.split(), capture_output=True)
 
     if proc.returncode:
-        logging.warning(proc.stderr.decode('utf-8').rstrip())
+        logging.warning(proc.stderr.decode("utf-8").rstrip())
 
-    output = proc.stdout.decode('utf-8').rstrip()
+    output = proc.stdout.decode("utf-8").rstrip()
 
     if not quiet:
         logging.info(output)
@@ -29,44 +29,50 @@ def run_cmd(cmd, quiet=False):
 
 
 def repo_default_branch(repo):
-    '''Programmatically obtain a repo's default branch for dynamic repo checkouts'''
+    """Programmatically obtain a repo's default branch for dynamic repo checkouts"""
     prev_dir = os.getcwd()
     os.chdir(repo.working_dir)
 
-    remote_info = cmd.Git().remote('show', 'origin')
-    default_branch_name = remote_info.split('\n')[3].split()[-1]
+    remote_info = cmd.Git().remote("show", "origin")
+    default_branch_name = remote_info.split("\n")[3].split()[-1]
 
     os.chdir(prev_dir)
     return default_branch_name
 
 
 def clone_repo(remote, path, overwrite=True):
-    '''Clone down a github repo to a path and a reference to that directory'''
+    """Clone down a github repo to a path and a reference to that directory"""
 
     if Path(path).is_dir() and overwrite:
-        logging.info(f'Removing folder that already exists: {path}')
+        logging.info(f"Removing folder that already exists: {path}")
         shutil.rmtree(path)
 
-    logging.info(f'cloning {remote} to {path}')
+    logging.info(f"cloning {remote} to {path}")
     repo = Repo.clone_from(remote, path)
 
     main_branch = repo_default_branch(repo)
     repo.git.checkout(main_branch)
-    logging.info(f'pulling {main_branch} at {path}')
+    logging.info(f"pulling {main_branch} at {path}")
     repo.remotes.origin.pull()
     return path, repo
 
 
 def config_token_authorization(repo, token):
-    '''
+    """
     Handle all 3 git URL styles as authenticated HTTPS URLs using a personal access token (PAT).
 
     This is the Simplest Bullet™ strategy described here:
     https://coolaj86.com/articles/vanilla-devops-git-credentials-cheatsheet/
-    '''
-    repo.config_writer().set_value(f'url "https://api:{token}@github.com/"', "insteadOf", "https://github.com/").release()
-    repo.config_writer().set_value(f'url "https://ssh:{token}@github.com/"', "insteadOf", "ssh://git@github.com/").release()
-    repo.config_writer().set_value(f'url "https://git:{token}@github.com/"', "insteadOf", "git@github.com:").release()
+    """
+    repo.config_writer().set_value(
+        f'url "https://api:{token}@github.com/"', "insteadOf", "https://github.com/"
+    ).release()
+    repo.config_writer().set_value(
+        f'url "https://ssh:{token}@github.com/"', "insteadOf", "ssh://git@github.com/"
+    ).release()
+    repo.config_writer().set_value(
+        f'url "https://git:{token}@github.com/"', "insteadOf", "git@github.com:"
+    ).release()
 
 
-__all__ = ['clone_repo', 'run_cmd']
+__all__ = ["clone_repo", "run_cmd"]
--- hubcap/release_carrier.py
+++ hubcap/release_carrier.py
@@ -1,4 +1,4 @@
-'''Interface for dispatching updates to packages back to github'''
+"""Interface for dispatching updates to packages back to github"""
 
 import json
 import requests
@@ -10,9 +10,9 @@ from git.remote import Remote
 
 
 def make_pr(org, repo, head, user_creds, url, pr_strategy, base="main"):
-    '''Create POST content which in turns create a hub new-version PR'''
-    user = user_creds['name']
-    token = user_creds['token']
+    """Create POST content which in turns create a hub new-version PR"""
+    user = user_creds["name"]
+    token = user_creds["token"]
     title = pr_strategy.pull_request_title(org, repo)
     body = "Auto-bumping from new release at https://github.com/{}/{}/releases".format(org, repo)
     maintainer_can_modify = True
@@ -20,35 +20,39 @@ def make_pr(org, repo, head, user_creds, url, pr_strategy, base="main"):
 
 
 def post_pr(title, head, base, body, maintainer_can_modify, user, token, url):
-    '''Create POST content which in turns create a hub new-version PR'''
+    """Create POST content which in turns create a hub new-version PR"""
     body = {
         "title": title,
         "head": head,
         "base": base,
         "body": body,
-        "maintainer_can_modify": maintainer_can_modify
+        "maintainer_can_modify": maintainer_can_modify,
     }
     body = json.dumps(body)
 
-    response = requests.post(url, data=body, headers={'Content-Type': 'application/json'}, auth=(user, token))
+    response = requests.post(
+        url, data=body, headers={"Content-Type": "application/json"}, auth=(user, token)
+    )
     response.raise_for_status()
 
 
 def get_open_pr_titles(org_name, package_name, user_creds):
-    '''Prevents opening duplicate PRs for currently open versions'''
-    url = f'https://api.github.com/repos/{org_name}/{package_name}/pulls?state=open'
+    """Prevents opening duplicate PRs for currently open versions"""
+    url = f"https://api.github.com/repos/{org_name}/{package_name}/pulls?state=open"
 
-    user = user_creds.get('name', None)
-    token = user_creds.get('token', None)
+    user = user_creds.get("name", None)
+    token = user_creds.get("token", None)
     pr_titles = []
 
     try:
         response = requests.get(url, auth=(user, token))
         response.raise_for_status()
-        pr_titles = [pr['title'] for pr in response.json()]
+        pr_titles = [pr["title"] for pr in response.json()]
     except Exception as e:
         helper.logging.error(e)
-        helper.logging.info(f"Is the {org_name}/{package_name} repository visible to the token's user?")
+        helper.logging.info(
+            f"Is the {org_name}/{package_name} repository visible to the token's user?"
+        )
         helper.logging.info("Does the token have applicable scopes (repo, workflow)?")
         exit(1)
 
@@ -56,45 +60,67 @@ def get_open_pr_titles(org_name, package_name, user_creds):
 
 
 def is_open_pr(prs, org_name, pkg_name):
-    return any('{}/{}'.format(org_name, pkg_name) in pr for pr in prs)
+    return any("{}/{}".format(org_name, pkg_name) in pr for pr in prs)
 
 
 def get_org_repo(remote_url: str) -> str:
-    '''Parse the organization and repository from a GitHub remote URL.'''
-    *_, target_org, target_pkg = remote_url.split('/')
+    """Parse the organization and repository from a GitHub remote URL."""
+    *_, target_org, target_pkg = remote_url.split("/")
     # Strip off "git@github.com:" from the beginning of the organization name
     target_org = target_org.replace("git@github.com:", "")
     # Strip off ".git" from the end of the package name
-    target_pkg_name = target_pkg[:-len('.git')]
+    target_pkg_name = target_pkg[: -len(".git")]
 
     return target_org, target_pkg_name
 
 
-def open_new_prs(target_repo_path, remote_url, branches, user_creds, push_branches, pull_request_url, pr_strategy, default_branch='main'):
-    '''Expects: {branch_name: hashmap of branch info} and {user_name, access token}
-    will push prs up to a github remote'''
+def open_new_prs(
+    target_repo_path,
+    remote_url,
+    branches,
+    user_creds,
+    push_branches,
+    pull_request_url,
+    pr_strategy,
+    default_branch="main",
+):
+    """Expects: {branch_name: hashmap of branch info} and {user_name, access token}
+    will push prs up to a github remote"""
 
     target_repo = Repo(target_repo_path)
-    if not Remote(target_repo, 'hub').exists():
-        target_repo.create_remote('hub', url=remote_url)
+    if not Remote(target_repo, "hub").exists():
+        target_repo.create_remote("hub", url=remote_url)
 
     target_org, target_pkg_name = get_org_repo(remote_url)
     open_pr_titles = get_open_pr_titles(target_org, target_pkg_name, user_creds)
 
-    pr_branches = { name: info for name, info in branches.items()
-                    if not is_open_pr(name, info['org'], info['repo'])}
+    pr_branches = {
+        name: info
+        for name, info in branches.items()
+        if not is_open_pr(name, info["org"], info["repo"])
+    }
 
     for name, info in branches.items():
         if name not in pr_branches.keys():
-            helper.logging.info("PR is already open for {}/{}. Skipping.".format(info['org'], info['repo']))
+            helper.logging.info(
+                "PR is already open for {}/{}. Skipping.".format(info["org"], info["repo"])
+            )
 
     for branch, info in pr_branches.items():
         target_repo.git.checkout(branch)
-        target_repo.git.fetch('hub')
+        target_repo.git.fetch("hub")
 
         if push_branches:
             helper.logging.info(f"Pushing and PRing branch {branch}")
-            origin = target_repo.git.push('origin', branch)
-            make_pr(info['org'], info['repo'], branch, user_creds, pull_request_url, pr_strategy, base=default_branch)
+            origin = target_repo.git.push("origin", branch)
+            make_pr(
+                info["org"],
+                info["repo"],
+                branch,
+                user_creds,
+                pull_request_url,
+                pr_strategy,
+                base=default_branch,
+            )
         else:
             helper.logging.info(f"Not pushing and PRing branch {branch}")

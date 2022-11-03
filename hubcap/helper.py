dee98c437bf2a9f01082d639c16a7e03d9c426c6
Doug Beatty <44704949+dbeatty10@users.noreply.github.com>
2022-11-03 14:25:50 -0600

    Code formatting (#184)
    * Fix End of Files
    
    * pre-commit check for code formatting using black
    
    * Reformatting via black

--- hubcap/helper.py
+++ hubcap/helper.py
@@ -1,4 +1,4 @@
-'''Environment setup and state generation for hubcap'''
+"""Environment setup and state generation for hubcap"""
 
 import collections
 import datetime
@@ -15,20 +15,20 @@ from records import *
 NOW = int(datetime.datetime.now().timestamp())
 
 logging.basicConfig(
-    format='%(asctime)s %(levelname)-8s %(message)s',
+    format="%(asctime)s %(levelname)-8s %(message)s",
     level=logging.INFO,
-    datefmt='%Y-%m-%d %H:%M:%S'
+    datefmt="%Y-%m-%d %H:%M:%S",
 )
 
 
 def build_config():
-    '''Pull the config env variable which holds github secrets'''
-    return json.loads(os.environ['CONFIG'])
+    """Pull the config env variable which holds github secrets"""
+    return json.loads(os.environ["CONFIG"])
 
 
 def load_package_maintainers():
-    '''Hub's state determined by packages and their maintainers listed in hub.json'''
-    with open('hub.json', 'r') as hub_stream, open('exclusions.json') as excluded_stream:
+    """Hub's state determined by packages and their maintainers listed in hub.json"""
+    with open("hub.json", "r") as hub_stream, open("exclusions.json") as excluded_stream:
         org_pkg_index = json.load(hub_stream)
         # Mila: we should strive to periodically remove dead packages from hub.json
         excluded_pkgs_index = json.load(excluded_stream)
@@ -47,22 +47,22 @@ def load_package_maintainers():
 
 
 def build_pkg_version_index(hub_path):
-    '''traverse the hub repo and load all versions for every package of every org into memory'''
+    """traverse the hub repo and load all versions for every package of every org into memory"""
 
     # store previous path for easy return at function exit
     prev_path = os.getcwd()
     os.chdir(hub_path)
 
     maintainer_package_map = {
-        maintainer: os.listdir(Path('data') / Path('packages') / Path(maintainer))
-        for maintainer in os.listdir(Path('data') / Path('packages'))
+        maintainer: os.listdir(Path("data") / Path("packages") / Path(maintainer))
+        for maintainer in os.listdir(Path("data") / Path("packages"))
     }
 
     package_version_index = {
         (package, maintainer): [
             # sublist of all versions checked into hub
-            os.path.basename(f)[:-len('.json')]  # include semver in filename only
-            for f in (Path('data') / 'packages'/ maintainer / package).glob('*/*.json')
+            os.path.basename(f)[: -len(".json")]  # include semver in filename only
+            for f in (Path("data") / "packages" / maintainer / package).glob("*/*.json")
         ]
         for maintainer in maintainer_package_map.keys()
         for package in maintainer_package_map[maintainer]
--- hubcap/hubcap.py
+++ hubcap/hubcap.py
@@ -21,21 +21,21 @@ from records import IndividualPullRequests, ConsolididatedPullRequest
 # == Build global state
 # ==
 
-logging.info('preparing script state')
+logging.info("preparing script state")
 config = helper.build_config()
 
 github_org = config.get("org", "dbt-labs")
 github_repo = config.get("repo", "hub.getdbt.com")
 push_branches = config.get("push_branches", True)
 one_branch_per_repo = config.get("one_branch_per_repo", True)
-user_config = config.get('user', {})
-GITHUB_USERNAME = user_config.get('name', 'dbt-hubcap')
-GITHUB_EMAIL = user_config.get('email', 'drew@fishtownanalytics.com')
-TOKEN = user_config.get('token')
-user_creds = {'name': GITHUB_USERNAME, 'token': TOKEN}
+user_config = config.get("user", {})
+GITHUB_USERNAME = user_config.get("name", "dbt-hubcap")
+GITHUB_EMAIL = user_config.get("email", "drew@fishtownanalytics.com")
+TOKEN = user_config.get("token")
+user_creds = {"name": GITHUB_USERNAME, "token": TOKEN}
 REMOTE = f"https://github.com/{github_org}/{github_repo}.git"
 PULL_REQUEST_URL = f"https://api.github.com/repos/{github_org}/{github_repo}/pulls"
-git_tmp = 'target'
+git_tmp = "target"
 TMP_DIR = Path(git_tmp).resolve()
 PACKAGE_MAINTAINERS = helper.load_package_maintainers()
 
@@ -60,43 +60,47 @@ HUB_VERSION_INDEX = helper.build_pkg_version_index(hub_dir_path)
 # = Determine new package versions
 # =
 
-logging.info('cloning package repos')
+logging.info("cloning package repos")
 package.clone_package_repos(PACKAGE_MAINTAINERS, TMP_DIR)
 
-logging.info('collecting the new version tags for packages checked into hub')
-update_tasks = package.get_update_tasks(PACKAGE_MAINTAINERS, HUB_VERSION_INDEX, TMP_DIR, github_repo)
+logging.info("collecting the new version tags for packages checked into hub")
+update_tasks = package.get_update_tasks(
+    PACKAGE_MAINTAINERS, HUB_VERSION_INDEX, TMP_DIR, github_repo
+)
 
 # =
 # = Create new specs and commit them to separate branches in hub
 # =     (stateful operations from here on)
 
-logging.info('preparing branches for packages with versions to be added')
+logging.info("preparing branches for packages with versions to be added")
 # branches exist inside the repo located at {TMP_DIR}/{github_repo} (which by default is target/hub.getdbt.com)
-new_branches = package.commit_version_updates_to_hub(update_tasks, hub_dir_path, pr_strategy, default_branch=default_branch)
+new_branches = package.commit_version_updates_to_hub(
+    update_tasks, hub_dir_path, pr_strategy, default_branch=default_branch
+)
 
 # =
 # = Add a branch with no commits to confirm that pushing works correctly
 # =
 
-branch_name = f'bump-test-{helper.NOW}'
+branch_name = f"bump-test-{helper.NOW}"
 
 main_dir = Path(TMP_DIR) / github_repo
 os.chdir(main_dir)
-completed_subprocess = subprocess.run(['git', 'checkout', '-q', '-b', branch_name])
+completed_subprocess = subprocess.run(["git", "checkout", "-q", "-b", branch_name])
 if completed_subprocess.returncode == 128:
-    git_helper.run_cmd(f'git checkout -q {branch_name}')
+    git_helper.run_cmd(f"git checkout -q {branch_name}")
 
 # Commit an empty file
-with open(branch_name, 'w') as fp:
+with open(branch_name, "w") as fp:
     pass
-git_helper.run_cmd('git add -A')
-subprocess.run(args=['git', 'commit', '-am', 'Test commit'], capture_output=True)
+git_helper.run_cmd("git add -A")
+subprocess.run(args=["git", "commit", "-am", "Test commit"], capture_output=True)
 
 # Reset back to the default branch
-git_helper.run_cmd(f'git checkout -q {default_branch}')
+git_helper.run_cmd(f"git checkout -q {default_branch}")
 
 # Add this branch to the list
-new_branches[branch_name] = {'org': 'dbt-labs', 'repo': 'hub.getdbt.com'}
+new_branches[branch_name] = {"org": "dbt-labs", "repo": "hub.getdbt.com"}
 
 # =
 # = push new branches, if there are any
@@ -104,4 +108,13 @@ new_branches[branch_name] = {'org': 'dbt-labs', 'repo': 'hub.getdbt.com'}
 
 logging.info("Pushing branches: {}".format(list(new_branches.keys())))
 if new_branches:
-    release_carrier.open_new_prs(hub_dir_path, REMOTE, new_branches, user_creds, push_branches, PULL_REQUEST_URL, pr_strategy, default_branch=default_branch)
+    release_carrier.open_new_prs(
+        hub_dir_path,
+        REMOTE,
+        new_branches,
+        user_creds,
+        push_branches,
+        PULL_REQUEST_URL,
+        pr_strategy,
+        default_branch=default_branch,
+    )
